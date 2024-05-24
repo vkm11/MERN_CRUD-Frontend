c6c7e061281f8e6f58b3a62ad5701166c7a6389b
vijaykumar <vijay1111mane@gmail.com>
2024-05-24 10:28:43 +0530

    update

--- src/pages/Parents.js
+++ src/pages/Parents.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from "../components/Layouts/Layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faPlus, faPenToSquare, faTrashCan} from '@fortawesome/free-solid-svg-icons';
+
+import axios from "axios";
+
 function Parents() {
-    const [userForm, setUserForm] = useState({
+    const [parentForm, setParentForm] = useState({
         firstName: "",
-        lastName: "",
+        nickName: "",
         status: "1",
     });
-    const [isChecked, setIsChecked] = useState(true);
-    const[hideStatus, setHideStatus] = useState(false)
 
+    const [parentGet, setParentGet] = useState([])
+    const [successMsg, setSuccessMsg] = useState("")
+    const [isChecked, setIsChecked] = useState(true);
+    const [hideStatus, setHideStatus] = useState(false)
+    const [selectedParent, setSelectedParent] = useState(null)
+
+    const [addFormDiv, setAddFormDiv] = useState(true)
+    const [searchDiv, setSearchDiv] = useState(false)
 
     const [touchedFields, setTouchedFields] = useState({
         firstName: false,
-        lastName: false,
+        nickName: false,
         status: true
     });
     const [errors, setErrors] = useState({});
@@ -32,30 +41,30 @@ function Parents() {
     };
 
 
-    const toggleSearchForm = () =>{
-        setHideStatus(true)
-        setUserForm({
-            firstName: "",
-            lastName: "",
-            status: ""
-        })
+    const toggleSearchForm = () => {
+        setAddFormDiv(false)
+        setSearchDiv(true)
+        
     }
-    const toggleParentForm = () =>{
+    const toggleParentForm = () => {
+        setAddFormDiv(true)
+        setSearchDiv(false)
+        setSelectedParent(true)
         setHideStatus(false)
         setIsChecked(true)
-        setUserForm({
+        setParentForm({
             firstName: "",
-            lastName: "",
+            nickName: "",
             status: "1"
         })
     }
 
     const handleFirstNameChange = (e) => {
         const value = e.target.value;
-        setUserForm(prevState => ({
+        setParentForm(prevState => ({
             ...prevState,
             firstName: value,
-            lastName: isChecked ? value : prevState.lastName
+            nickName: isChecked ? value : prevState.nickName
         }));
         setTouchedFields(prevState => ({
             ...prevState,
@@ -68,38 +77,38 @@ function Parents() {
         }));
     };
 
-    const handleLastNameChange = (e) => {
-        setUserForm(prevState => ({
+    const handleNickNameChange = (e) => {
+        setParentForm(prevState => ({
             ...prevState,
-            lastName: e.target.value
+            nickName: e.target.value
         }));
         setTouchedFields(prevState => ({
             ...prevState,
-            lastName: true
+            nickName: true
         }));
     };
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
         if (isChecked) {
-            setUserForm(prevState => ({
+            setParentForm(prevState => ({
                 ...prevState,
-                lastName: ""
+                nickName: ""
             }));
         } else {
-            setUserForm(prevState => ({
+            setParentForm(prevState => ({
                 ...prevState,
-                lastName: userForm.firstName
+                nickName: parentForm.firstName
             }));
         }
         setTouchedFields(prevState => ({
             ...prevState,
-            lastName: true
+            nickName: true
         }));
     };
 
     const handleStatusChange = (e) => {
-        setUserForm(prevState => ({
+        setParentForm(prevState => ({
             ...prevState,
             status: e.target.value
         }));
@@ -108,11 +117,25 @@ function Parents() {
             status: true
         }));
     };
+    const getData = (e) => {
+        axios
+            .get("http://localhost:4000/parent/")
+            .then((res) => {
+                setParentGet(res.data.data);
+                // setSearchResult(res.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    useEffect(() => {
+        getData()
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         toggleParentForm()
-        const firstNameError = validateFirstName(userForm.firstName);
+        const firstNameError = validateFirstName(parentForm.firstName);
         setErrors(prevErrors => ({
             ...prevErrors,
             firstName: firstNameError
@@ -121,20 +144,58 @@ function Parents() {
         if (firstNameError) {
             return;
         }
-      
+        axios
+            .post("http://localhost:4000/parent/create-parent", parentForm)
+            .then((res) => {
+                console.log(res.data);
+                setSuccessMsg(res.data.message)
+                setParentForm({
+                    firstName: "",
+                    nickName: "",
+                    status: ''
+                });
+                setErrors({});
+                setTimeout(() => {
+                    setSuccessMsg("")
+                    getData()
+                }, 1000)
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
 
-        console.log('Form submitted:', userForm);
+
+        console.log('Form submitted:', parentForm);
+     
     };
 
     const isFormValid = () => {
         if (isChecked) {
-            return userForm.firstName.trim() !== '' && userForm.status !== '' &&
+            return parentForm.firstName.trim() !== '' && parentForm.status !== '' &&
                 touchedFields.firstName && touchedFields.status && !errors.firstName;
         } else {
-            return userForm.firstName.trim() !== '' && userForm.lastName.trim() !== '' && userForm.status !== '' &&
-                touchedFields.firstName && touchedFields.lastName && touchedFields.status && !errors.firstName;
+            return parentForm.firstName.trim() !== '' && parentForm.nickName.trim() !== '' && parentForm.status !== '' &&
+                touchedFields.firstName && touchedFields.nickName && touchedFields.status && !errors.firstName;
         }
     };
+    const updateParent = (_id) => {
+        // Add update logic here
+        setHideStatus(true)
+        setIsChecked(true)
+     
+        // setShowSearchForm(false);
+        const selected = parentGet.find((parent) => parent._id === _id);
+        setSelectedParent(selected);
+        setParentForm({
+            firstName: selected.firstName,
+            nickName: selected.nickName,
+            status: selected.status,
+        })
+    };
+    const deleteParent = (_id) => {
+
+    }
+
 
     return (
         <>
@@ -150,6 +211,12 @@ function Parents() {
                                 <button className='addBtn' onClick={toggleParentForm}><FontAwesomeIcon icon={faPlus} /> Add Parents</button>
                             </div>
                         </div>
+                        {searchDiv && (
+                            <div>
+                                <p>Search works here</p>
+                            </div>
+                        )}
+                        { addFormDiv && (
                         <div>
                             <form onSubmit={handleSubmit}>
                                 <div className='row'>
@@ -157,7 +224,7 @@ function Parents() {
                                         <label className='form-label'>First Name</label>
                                         <input className='form-control'
                                             type="text"
-                                            value={userForm.firstName}
+                                            value={parentForm.firstName}
                                             onChange={handleFirstNameChange}
                                             onBlur={() => setTouchedFields(prevState => ({ ...prevState, firstName: true }))}
                                         />
@@ -166,7 +233,7 @@ function Parents() {
                                     <div className='col-sm-4'>
                                         <div className='d-flex justify-content-between align-items-center'>
                                             <div>
-                                                <label className='form-label'>Last Name</label>
+                                                <label className='form-label'>Nick Name </label>
                                             </div>
                                             <div>
                                                 <input
@@ -179,19 +246,19 @@ function Parents() {
                                         </div>
                                         <input className='form-control'
                                             type="text"
-                                            value={userForm.lastName}
-                                            onChange={handleLastNameChange}
-                                            onBlur={() => setTouchedFields(prevState => ({ ...prevState, lastName: true }))}
+                                            value={parentForm.nickName}
+                                            onChange={handleNickNameChange}
+                                            onBlur={() => setTouchedFields(prevState => ({ ...prevState, nickName: true }))}
                                             disabled={isChecked}
                                         />
-                                        {!isChecked && touchedFields.lastName && userForm.lastName.trim() === '' &&
-                                            <span className="text-danger">Last Name is required</span>}
+                                        {!isChecked && touchedFields.nickName && parentForm.nickName.trim() === '' &&
+                                            <span className="text-danger">Nickname  is required</span>}
                                     </div>
-                                    {hideStatus && ( <div className="col-sm-4">
+                                    {hideStatus && (<div className="col-sm-4">
                                         <label className='form-label'>Status</label>
                                         <select
                                             className="form-control"
-                                            value={userForm.status}
+                                            value={parentForm.status}
                                             onChange={handleStatusChange}
                                             onBlur={() => setTouchedFields(prevState => ({ ...prevState, status: true }))}
                                         >
@@ -199,18 +266,74 @@ function Parents() {
                                             <option value="0" className="text-danger">Inactive</option>
                                             <option value="1" className="text-primary">Active</option>
                                         </select>
-                                        {touchedFields.status && userForm.status === '' &&
+                                        {touchedFields.status && parentForm.status === '' &&
                                             <span className="text-danger">Status is required</span>}
                                     </div>
                                     )}
                                 </div>
                                 <br />
                                 <div className='text-end'>
-                                <button className='btn btn-primary' type="submit" disabled={!isFormValid()}>Submit</button>
+                                    <button className='btn btn-primary' type="submit" disabled={!isFormValid() }>{selectedParent ? 'Update' : 'Submit'}</button>
                                 </div>
                             </form>
+                                {successMsg && (<div>
+                                    {successMsg}
+                                </div>
+                                )}
+                        </div>
+                      
+                        )}
+                       
+                    </div>
+                    <div className="card p-2 mt-2">
+                        {/* {currentItems.length > 0 ? ( */}
+                        <div className="table-responsive">
+                            <table className="table table-bordered table-striped table-hover">
+                                <thead className="table-dark">
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Nick Name</th>
+                                        <th scope="col" className="text-center">Status</th>
+                                        <th scope="col" className="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="table-group-divider">
+                                    {parentGet.map((parent, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{parent.firstName}</td>
+                                                <td>{parent.nickName}</td>
+                                                <td>
+                                                    <div className='text-center'>
+                                                        {parent.status === 1 ? <span className="badge rounded-pill text-bg-success">Active</span> : <span className="badge rounded-pill text-bg-danger">Inactive</span>}
+                                                    </div>
+                                                </td>
+                                                <td className="text-center">
+                                                    <button
+                                                        className="btn btn-primary btn-sm me-2" onClick={() => updateParent(parent._id)}>
+                                                        <FontAwesomeIcon icon={faPenToSquare} />
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-danger btn-sm"
+                                                        onClick={() => deleteParent(parent._id)}>
+                                                        <FontAwesomeIcon icon={faTrashCan} />
+                                                    </button>
+                                                    {/* <span>
+                                                        {parent.status == 1 && (<a><FontAwesomeIcon icon={faToggleOn} size="2xl" /></a>)}
+                                                        {parent.status == 0 && (<a><FontAwesomeIcon icon={faToggleOff} size="2xl" /></a>)}
+                                                    </span> */}
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+
+                            </table>
                         </div>
                     </div>
+
                 </div>
             </Layout>
         </>
